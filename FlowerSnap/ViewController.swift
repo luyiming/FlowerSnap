2a18cf503dda9e161872bdf0ab639147be796624
luyiming <luyimingchn@gmail.com>
2018-11-12 16:04:14 +0800

    Initial Commit

---
+++ FlowerSnap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlowerSnap
+//
+//  Created by 陆依鸣 on 2018/11/12.
+//  Copyright © 2018 陆依鸣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FlowerSnapTests/FlowerSnapTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlowerSnapTests.swift
+//  FlowerSnapTests
+//
+//  Created by 陆依鸣 on 2018/11/12.
+//  Copyright © 2018 陆依鸣. All rights reserved.
+//
+
+import XCTest
+@testable import FlowerSnap
+
+class FlowerSnapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
